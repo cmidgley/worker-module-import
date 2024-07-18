5e8110038a22796e6f7755e7cb03e1abf15e45f9
cmidgley <chris@Koose.com>
2024-07-18 11:41:56 -0400

    Add comments

--- src/example/startWorker.js
+++ src/example/startWorker.js
@@ -1,9 +1,13 @@
 import Worker from 'worker'
 
 export async function startWorker() {
+    // this will successfully import the module, showing that the path itself is correct
     trace('in startWorker - importing "./worker.js"\n');
     const importedWorker = await import('./worker.js');
+
+    // this will fail to import the module (with or without prior importing of the module, as above)
     trace('import done; starting worker\n');
     const worker = new Worker('./worker.js');
+
     trace('worker started\n');
 }
